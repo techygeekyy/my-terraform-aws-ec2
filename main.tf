6f39c542246e214b9651053adc18d5af679ab871
Amol <theamrual@gmail.com>
2025-09-21 23:32:52 +0530

    Update main.tf
    AWS EC2 instances launching from GitHub

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-08982f1c5bf93d976" # Amazon Linux 2
+  instance_type = "t3.micro"
+  tags = {
+    Name = "GitHub to Terraform to AWS_EC2"
+  }
+}
 
